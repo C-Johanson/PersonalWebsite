7af31afc05caac44a7fa2c120a32df625b68e1a4
C-Johanson <C.Alx.Johanson@gmail.com>
2022-12-04 13:40:00 -0500

    deploy

--- my-site/next.config.js
+++ my-site/next.config.js
@@ -2,11 +2,12 @@
    * @type {import('next').NextConfig}
    */
  const nextConfig   = {
+  reactStrictMode: true,
   images: {
     loader: 'akamai',
     path: '',
   },
-  assetPrefix: './',
+  
 };
 
-export default nextConfig;
+module.exports = nextConfig
